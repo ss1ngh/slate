da20bbc0a50682d12a08e4385da8550f3529ca72
ss1ngh <singhshreyansh2354@gmail.com>
2026-02-14 20:00:58 +0530

    feat(shapes) : added lines, arrows and pencil

--- apps/slate/app/src/config/types.ts
+++ apps/slate/app/src/config/types.ts
@@ -1,20 +1,40 @@
-export type ShapeType = 'rect' | 'circle' | 'pencil' | 'line'
+export type ShapeType = 'rect' | 'circle' | 'pencil' | 'line' | 'arrow' | 'text';
 
 export interface BaseShape {
-    id : string;
-    type : ShapeType;
-    x : number;
-    y : number;
-    strokeColor : string;
-    strokeWidth : number;
+    id: string;
+    type: ShapeType;
+    x: number; // Starting X
+    y: number; // Starting Y
+    strokeColor: string;
+    strokeWidth: number;
 }
 
 export interface RectShape extends BaseShape {
-    type : 'rect';
-    width : number;
-    length : number;
+    type: 'rect';
+    width: number;
+    height: number;
 }
 
-export interface Circle extends BaseShape {
-    type : 'circle'
+export interface CircleShape extends BaseShape {
+    type: 'circle';
+    radius: number;
 }
+
+export interface LineShape extends BaseShape {
+    type : 'line' | 'arrow'
+    endX : number;
+    endY : number;
+}
+
+export interface PencilShape extends BaseShape {
+    type : 'pencil';
+    points : {x : number, y: number}[]; //array of every mouse movement
+}
+
+export interface TextShape extends BaseShape {
+  type: 'text';
+  text: string;
+  fontSize: number;
+}
+
+export type Shape = RectShape | CircleShape | PencilShape | LineShape | TextShape;
